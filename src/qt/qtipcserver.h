907c4bf8dc19dd604db6294ff6b71833ae86d898
tradebits <tradebits@email.com>
2016-11-14 13:37:24 +0200

    Basic Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Tradebits-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TradebitsURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
